5c303704a08049d89369d8097bb22591e7072fdd
YousLam <ys5128837@gmail.com>
2025-01-14 13:50:35 +0100

    Fex some bugs.

--- api/serializers.py
+++ api/serializers.py
@@ -2,4 +2,4 @@ from rest_framework import serializers
 
 class TranslationRequestSerializer(serializers.Serializer):
     text = serializers.CharField()
-    target_lang = serializers.CharField()
+    target_language = serializers.CharField()
--- api/service.py
+++ api/service.py
@@ -4,17 +4,17 @@ class TranslationSrevice:
     _cache = {}
 
     @staticmethod
-    def translate_text(text:str, target_lang:str) -> dict:
-        if (text, target_lang) in TranslationSrevice._cache:
-            return TranslationSrevice._cache[(text, target_lang)]
+    def translate_text(text:str, target_language:str) -> dict:
+        if (text, target_language) in TranslationSrevice._cache:
+            return TranslationSrevice._cache[(text, target_language)]
         else:
-            translator = Translator(to_lang=target_lang)
+            translator = Translator(to_lang=target_language)
             translated_text = translator.translate(text)
 
-            TranslationSrevice._cache[(text, target_lang)] = {
+            TranslationSrevice._cache[(text, target_language)] = {
                 "original_text": text,
                 "translated_text": translated_text,
-                "target_lanuange": target_lang,
+                "target_language": target_language,
             }
 
-            return TranslationSrevice._cache[(text, target_lang)]
+            return TranslationSrevice._cache[(text, target_language)]
